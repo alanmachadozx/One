10546c53d1cf863281d03af77e71234b10c7e856
Alan Machado <alanpinheirorj22@gmail.com>
2026-08-21 12:23:44 -0300

    add: create audio_record function

--- src/listener.py
+++ src/listener.py
@@ -4,18 +4,15 @@ import sounddevice as sd
 from actions import *
 
 comands_execute = Actions()
-def start_listerning(q):
-        def callback(indata, frames, time, status):
-            if status:
-                print(status, file=sys.stderr)
-            q.put(bytes(indata))
-                    
-        try:
-            with sd.RawInputStream(samplerate= 16000, blocksize= 8000, dtype= "int16", channels= 1, callback= callback):
-                while True:
-                    data = q.get()
-        
-                        
-        
-        except KeyboardInterrupt:
-            print("The program is over")
+def start_listerning(q, samplerate, duration):
+
+    def audio_record():
+        print("Recordinng...")
+        with sd.InputStream(samplerate= samplerate, channels= 1, dtype = "float32") as stream:
+            # return a tuple with 2 elements, read(data, overflowed), the [0] is used for select just data
+            audio = stream.read(int(samplerate * duration))[0]
+         
+        sd.wait()
+        #converts the two-dimensional(2D) audio matrix into a one-dimensional (1D) vector
+        # the faster-whisper library requires this format
+        return audio.flatten()
--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ import queue
 import sounddevice as sd
 from  listener import *
 from faster_whisper import WhisperModel
+import webrtcvad
 
 q = queue.Queue()
+SAMPLERATE = 16000
+DURATION = 7
+FRAMEDURATION = 30 #ms
+FRAME_SIZE = int(SAMPLERATE * FRAMEDURATION/ 1000)
 
+vad = webrtcvad.Vad(3) #set aggressiveness mode, where 3 is the most agressive
 model = WhisperModel("tiny.en", device= "cpu", compute_type= "int8")
 segments = model.transcribe("test.wav", language= "en")
 
-start_listerning(q)
+start_listerning(q, SAMPLERATE, DURATION)
