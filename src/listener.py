95beed5fcc1e4e1edf845532da21c977b160aaf3
Alan Machado <alanpinheirorj22@gmail.com>
2026-08-21 18:41:44 -0300

    add: integrated the speech component with the command component

--- src/listener.py
+++ src/listener.py
@@ -2,9 +2,11 @@ import json
 import sys
 import sounddevice as sd
 from actions import *
+import numpy as np
+from faster_whisper import WhisperModel
 
 comands_execute = Actions()
-def start_listerning(q, samplerate, duration):
+def start_listerning(q, samplerate, duration, frame_size, vad, model):
 
     def audio_record():
         print("Recordinng...")
@@ -16,3 +18,36 @@ def start_listerning(q, samplerate, duration):
         #converts the two-dimensional(2D) audio matrix into a one-dimensional (1D) vector
         # the faster-whisper library requires this format
         return audio.flatten()
+
+    def contains_speech(audio):
+        pcm = (audio * 32768).astype(np.int16).tobytes()
+
+        for i in range(0, len(pcm), frame_size * 2):
+            frame = pcm[i: i + frame_size * 2]
+
+            if len(frame) < frame_size * 2:
+                break
+
+            if vad.is_speech(frame, samplerate):
+                return True
+
+        return False
+
+    def transcribe_audio(audio):
+        segments, _ = model.transcribe(audio, language= "en")
+        text = None
+        
+        for segment in segments:
+            print(f"{segment.text.strip()}")
+            text = segment.text.strip()
+
+        return text
+
+    try:
+        while True:
+            if contains_speech(audio_record()):
+                text = transcribe_audio(audio_record())
+                comands_execute.process(text)
+
+    except KeyboardInterrupt:
+        print("program finished")
--- src/main.py
+++ src/main.py
@@ -1,17 +1,14 @@
 import queue
-import sounddevice as sd
 from  listener import *
-from faster_whisper import WhisperModel
 import webrtcvad
 
 q = queue.Queue()
 SAMPLERATE = 16000
-DURATION = 7
+DURATION = 5
 FRAMEDURATION = 30 #ms
 FRAME_SIZE = int(SAMPLERATE * FRAMEDURATION/ 1000)
 
 vad = webrtcvad.Vad(3) #set aggressiveness mode, where 3 is the most agressive
 model = WhisperModel("tiny.en", device= "cpu", compute_type= "int8")
-segments = model.transcribe("test.wav", language= "en")
 
-start_listerning(q, SAMPLERATE, DURATION)
+start_listerning(q, SAMPLERATE, DURATION, FRAME_SIZE, vad, model)
