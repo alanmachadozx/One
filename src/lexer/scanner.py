a08aedfef5a274ff981910473572ea2f7234cc14
Alan Machado <alanpinheirorj22@gmail.com>
2026-09-12 14:21:44 -0300

    add: added postion functions

--- src/lexer/scanner.py
+++ src/lexer/scanner.py
@@ -5,3 +5,30 @@ class Scanner:
         self.start = 0
         self.line = 1
         self.tokens = []
+
+        def finished(self):
+            return self.current >= len(self.text)
+
+        def advance(self):
+            self.current += 1
+            return self.text[self.current - 1]
+
+        def previous(self):
+            return self.text[self.current - 1]
+
+        def peek(self):
+            if self.finished():
+                return '\0'
+            return self.text[self.current]
+
+        def peek_next(self):
+            if self.current + 1 >= len(self.text):
+                return '\0'
+            return self.text[self.current + 1]
+
+        def scan(self):
+            while not self.finished():
+                self.start = self.current
+                
+
+                
