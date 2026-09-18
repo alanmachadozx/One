2af88f11930a8e65bd58447a24e2cbd81a373357
Alan Machado <alanpinheirorj22@gmail.com>
2026-09-18 18:58:52 -0300

    add: splits the structure into action + target

--- src/ast/checker.py
+++ src/ast/checker.py
@@ -5,9 +5,11 @@ command_execute = Actions()
 
 #if it is a SequenceAction, check the left and right nodes
 def ast_checker(node: CommandExpr):
+    
     if isinstance(node, SequenceAction):
         ast_checker(node.left)
         ast_checker(node.right)
 
-    if isinstance(node, SingleAction):
-        command_execute.process(node.text)
+    if isinstance(node, SingleAction) and node.action:
+        true_command = node.action + " " + node.target
+        command_execute.process(true_command)
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,11 +1,12 @@
-from src.lexer.token import Token
+from src.lexer.token import Token, TokenType
 
 class CommandExpr:
         pass
 
 class SingleAction(CommandExpr):
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, action: str | None, target: str):
+        self.target = target
+        self.action = action
 
 class SequenceAction(CommandExpr):
     def __init__(self, left: CommandExpr, operator: str, right: CommandExpr):
@@ -17,6 +18,9 @@ class Parser:
     def __init__(self, tokens: list[Token]):
         self.tokens = tokens
         self.current_token = 0
+        self.actions = { #picks all token types except AND and IDENTIFIER
+            t for t in TokenType if t not in (TokenType.AND, TokenType.IDENTIFIER)
+        }
 
     def peek(self) -> Token:
         return self.tokens[self.current_token]
@@ -40,22 +44,37 @@ class Parser:
             return True
         return False
 
+    def is_action(self):
+        if self.current_token >= len(self.tokens):
+            return False
+        return self.peek().type in self.actions
+
     def parse(self):
         buffer = []
 
         while self.current_token < len(self.tokens) and not self.check("and"):
             buffer.append(self.peek().lexeme)
             self.advance()
-
+            
         return buffer
 
     def parse_command(self) -> CommandExpr:
-        left = SingleAction(" ".join(self.parse()))
+        action = None
+        target = " "
+        
+        if self.is_action():
+            action = self.peek().lexeme #splits the structure into {"action", "target"}
+            self.advance()
+            target = " ".join(self.parse())
+        else:
+            target = " ".join(self.parse())
+            
+        left = SingleAction(action, target)
 
         if self.match("and"):
             operator = self.previous().lexeme
             right = self.parse_command()
             
             return SequenceAction(left, operator, right)
-            
+        
         return left
