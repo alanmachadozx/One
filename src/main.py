a12d41dc67ebd1652f71079c0f71b42b52761cd0
Alan Machado <alanpinheirorj22@gmail.com>
2026-09-21 19:30:23 -0300

    add: added background color

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
 
 from src.listener import *
+import slint
+import sys
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+slint_path = os.path.join(path, '..', 'front-end', 'app-window.slint')
+
+#load the ui
+ui = slint.load_file(slint_path)
+
+app = ui.MainWindow()
+app.run()
 
 start_listerning()
